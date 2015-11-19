70e0ee31bbcb6ca10f11c18c27d9bea69811afbb
Andraž Krašček <andraz.krascek@gmail.com>
2015-11-19 19:03:59 +0100

    puppet deploy module require php and composer install or update composer

---
+++ puppet/modules/deploy/manifests/init.pp
@@ -0,0 +1,21 @@
+class deploy {
+
+    require php
+    require composer
+
+    exec { 'deploy-install':
+        command => 'composer install',
+        cwd => '/vagrant/server',
+        environment => ['COMPOSER_HOME=/vagrant/server'],
+        path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ],
+        unless => 'test -e composer.lock'
+    }
+
+    exec { 'deploy-update':
+        command => 'composer update',
+        cwd => '/vagrant/server',
+        environment => ['COMPOSER_HOME=/vagrant/server'],
+        path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ],
+        onlyif => 'test -e composer.lock'
+    }
+}
