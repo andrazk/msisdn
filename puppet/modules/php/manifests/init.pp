0c0715679ddb007cee04766098b7a54f5104ed64
Andraž Krašček <andraz.krascek@gmail.com>
2015-11-19 19:02:48 +0100

    puppet php56 module add repository, update and install php5 package

---
+++ puppet/modules/php/manifests/init.pp
@@ -0,0 +1,38 @@
+class php {
+
+  file { '/etc/apt/apt.conf.d/99auth':
+    owner => root,
+    group => root,
+    content => 'APT::Get::AllowUnauthenticated yes;',
+    mode =>644;
+  }
+
+  exec { 'add-repo':
+    command => 'add-apt-repository ppa:ondrej/php5-5.6',
+    path => '/usr/bin',
+    require => File['/etc/apt/apt.conf.d/99auth']
+  }
+
+  exec { 'update-first':
+    command => 'apt-get update',
+    path => '/usr/bin',
+    require => Exec['add-repo'],
+  }
+
+  package { 'python-software-properties':
+    ensure => present,
+    require => Exec['update-first'],
+  }
+
+  exec { 'update-second':
+    command => 'apt-get update',
+    path => '/usr/bin',
+    require => Package['python-software-properties'],
+  }
+
+  package { 'php5':
+    ensure => present,
+    install_options => ['--allow-unauthenticated', '-f'],
+    require => Exec['update-second'],
+  }
+}
