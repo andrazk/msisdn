328aba527b210c9d116fb8049c26daceba4995da
Andraž Krašček <andraz.krascek@gmail.com>
2015-11-20 07:00:38 +0100

    include composer.loc in version control autoload msidn pacakge

--- puppet/modules/deploy/manifests/init.pp
+++ puppet/modules/deploy/manifests/init.pp
@@ -8,14 +8,14 @@ class deploy {
         cwd => '/vagrant/server',
         environment => ['COMPOSER_HOME=/vagrant/server'],
         path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ],
-        unless => 'test -e composer.lock'
+        "unless => 'test -e composer.lock'
     }
 
-    exec { 'deploy-update':
-        command => 'composer update',
-        cwd => '/vagrant/server',
-        environment => ['COMPOSER_HOME=/vagrant/server'],
-        path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ],
-        onlyif => 'test -e composer.lock'
-    }
+    # exec { 'deploy-update':
+    #     command => 'composer update',
+    #     cwd => '/vagrant/server',
+    #     environment => ['COMPOSER_HOME=/vagrant/server'],
+    #     path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ],
+    #     onlyif => 'test -e composer.lock'
+    # }
 }
