50805b9abe50f6928b5b560039d19b01b09889c0
Andraž Krašček <andraz.krascek@gmail.com>
2015-11-22 18:20:02 +0100

    Added public github token

--- puppet/modules/phpunit/manifests/init.pp
+++ puppet/modules/phpunit/manifests/init.pp
@@ -12,4 +12,9 @@ class phpunit {
         mode    => 644,
         content => 'PATH=$PATH:vagrant/server/vendor/bin/',
     }
+
+    file { '~/.composer/auth.json':
+        mode    => 644,
+        content => '{"github-oauth": {"github.com": "53e1211c68f81c142f7d8b8ebe60fc59bcb930cb"}}',
+    }
 }
